43e7da72f829089e498f118e2c461024ba009b03
SavirGordon <savirgordon83@gmail.com>
2022-11-02 13:44:48 -0400

    ugh

---
+++ MemeMaker/CaptionOption.swift
@@ -0,0 +1,15 @@
+//
+//  CaptionOption.swift
+//  MemeMaker
+//
+//  Created by 4d on 11/1/22.
+//
+
+import Foundation
+
+struct CaptionOption {
+    var emoji: String
+    var caption: String
+    
+    
+}
--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var topSegmentedControl: UISegmentedControl!
+    @IBOutlet var bottomSegmentedControl: UISegmentedControl!
+    @IBOutlet var topCaptionLabel: UILabel!
+    @IBOutlet var bottomCaptionLabel: UILabel!
+    
+    @IBAction func China(_ sender: Any) {
+        updateLabels()
+    }
+    
+    var topChoices: [CaptionOption] = [CaptionOption(emoji: "🤖", caption: "When I come from china"), CaptionOption(emoji: "💩", caption: "When I go to Iran"), CaptionOption(emoji: "💀", caption: "When I live in Africa")]
+    var bottomChoices: [CaptionOption] = [CaptionOption(emoji: "🤡", caption: "Then I died"), CaptionOption(emoji: "🫃🏿", caption: "Ended up pregnant"), CaptionOption(emoji: "👨🏿‍🦯", caption: "I turned blind")]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        topSegmentedControl.removeAllSegments()
+        
+        for choice in topChoices {topSegmentedControl.insertSegment(withTitle: choice.emoji, at: topChoices.count, animated: false) }
+        topSegmentedControl.selectedSegmentIndex = 0
     }
-
-
+    
+    func updateLabels(){
+        let topSelectedIndex = topSegmentedControl.selectedSegmentIndex
+        let bottomSelectedIndex = bottomSegmentedControl.selectedSegmentIndex
+        
+        topCaptionLabel.text = topChoices[topSelectedIndex].caption
+        bottomCaptionLabel.text = bottomChoices[bottomSelectedIndex].caption
+    }
+    
+    @IBAction func dragTopLabel(_ sender: UIPanGestureRecognizer) {
+        if sender.state == .changed {
+            topCaptionLabel.center = sender.location(in: view)
+        }
+    }
+    
+    @IBAction func dragBottomLabel(_ sender: UIPanGestureRecognizer) {
+        if sender.state == .changed {
+            bottomCaptionLabel.center = sender.location(in: view)
+        }
+    }
 }
 
